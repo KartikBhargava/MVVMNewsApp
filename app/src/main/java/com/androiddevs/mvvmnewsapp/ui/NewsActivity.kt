b63aff788086e621db79883fd1bf9691360ce0c9
KartikBhargava <bhargava.kartik07@gmail.com>
2020-04-23 00:23:44 +0530

    Retrofit Fully Setup

--- app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/ui/NewsActivity.kt
@@ -12,7 +12,6 @@ class NewsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_news)
-
         bottomNavigationView.setupWithNavController(newsNavHostFragment.findNavController())
     }
 }
---
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -0,0 +1,8 @@
+package com.androiddevs.mvvmnewsapp.util
+
+class Constants {
+    companion object {
+        const val API_KEY = "6008399160524adeb4cf7347a02e1e19"
+        const val BASE_URL = "https;//newsapi.org"
+    }
+}
