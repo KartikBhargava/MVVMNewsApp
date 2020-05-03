738f3ae485526ebc3aa6b692c5d59775d34c9f0f
KartikBhargava <bhargava.kartik07@gmail.com>
2020-05-03 19:40:55 +0530

    WebView implemented

--- app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
+++ app/src/main/java/com/androiddevs/mvvmnewsapp/util/Constants.kt
@@ -2,7 +2,8 @@ package com.androiddevs.mvvmnewsapp.util
 
 class Constants {
     companion object {
-        const val API_KEY = "6008399160524adeb4cf7347a02e1e19"
-        const val BASE_URL = "https;//newsapi.org"
+        const val API_KEY = "d27efaee451c428c99cbcb37439e8700"
+        const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
